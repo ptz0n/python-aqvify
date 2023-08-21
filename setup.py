b3be705d5b124682db0c71f9968cc348ef5c8793
Erik Eng <erik@eng.se>
2023-08-21 19:16:37 +0200

    Add setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 setup(
     name='aqvify',
     version='0.1.0',
-    description='Read values from Aqvify through the official API.',
-    long_description='A python module for reading values of Aqvify through the official API.',
+    description='Read values from Aqvify through their API.',
+    long_description='A python module for reading values from Aqvify devices through the official API.',
     url='http://github.com/ptz0n/python-aqvify',
     author='Erik Eng',
     author_email='erik@eng.se',
